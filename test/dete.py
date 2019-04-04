5ee0768f26926710e915790e98936b934f37dfe3
Dj140 <33819388+dj140@users.noreply.github.com>
2019-04-04 21:13:31 +0800

    Update dete.py

--- test/dete.py
+++ test/dete.py
@@ -3,6 +3,11 @@
 __author__ = 'dj140'
 
 import sys 
+ros_path = '/opt/ros/kinetic/lib/python2.7/dist-packages'
+if ros_path in sys.path:
+    sys.path.remove(ros_path)
+import cv2
+sys.path.append('/opt/ros/kinetic/lib/python2.7/dist-packages')
 import numpy as np
 import cv2
 
